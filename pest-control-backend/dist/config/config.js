6ead2e40af5f0f0f6cfd4a36a33530a1dc9501ac
Your Name <aifasanaauk@gmail.com>
2026-07-16 19:57:56 +0530

    Load dotenv using absolute path to support Passenger environment on Namecheap

--- pest-control-backend/dist/config/config.js
+++ pest-control-backend/dist/config/config.js
@@ -3,11 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const path_1 = __importDefault(require("path"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const env = process.env.NODE_ENV ?? 'development';
 const isTest = env === 'test';
 if (!isTest) {
-    dotenv_1.default.config();
+    dotenv_1.default.config({ path: path_1.default.join(__dirname, '../../.env') });
 }
 const config = {
     env,
--- pest-control-backend/src/config/config.ts
+++ pest-control-backend/src/config/config.ts
@@ -1,10 +1,11 @@
+import path from 'path';
 import dotenv from 'dotenv';
 
 const env = process.env.NODE_ENV ?? 'development';
 const isTest = env === 'test';
 
 if (!isTest) {
-  dotenv.config();
+  dotenv.config({ path: path.join(__dirname, '../../.env') });
 }
 
 const config = {
