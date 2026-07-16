16b6afe45715bf141dfba0fc50558b4ee6cfae4f
Your Name <aifasanaauk@gmail.com>
2026-07-16 20:43:50 +0530

    Trigger Vercel build to apply updated VITE_API_URL settings

--- pest-control-frontend/src/main.tsx
+++ pest-control-frontend/src/main.tsx
@@ -5,6 +5,8 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
+// Trigger automatic Vercel rebuild to inject latest VITE_API_URL
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
